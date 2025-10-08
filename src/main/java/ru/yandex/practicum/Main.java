55d687a8370c7bd1f059dc2303903ce149e53d6a
ashipovalov <ashipovalov@gmail.com>
2025-10-08 20:21:32 +0300

    Коммит для создания конфликта

--- src/main/java/ru/yandex/practicum/Main.java
+++ src/main/java/ru/yandex/practicum/Main.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Я новый разработчик с темной темой");
 
         }
     }
